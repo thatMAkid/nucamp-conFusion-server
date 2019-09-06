716d5b736b761c5a5fe6b18c4cd62276267706f6
Morgan Anders <MorganAndersDP@gmail.com>
2019-09-06 05:00:31 -0700

    Passport Facebook

--- config.js
+++ config.js
@@ -1,4 +1,8 @@
 module.exports = {
     'secretKey': '12345-67890-09876-54321',
-    'mongoUrl': 'mongodb://localhost:27017/conFusion'
+    'mongoUrl': 'mongodb://localhost:27017/conFusion',
+    'facebook': {
+        clientId: '739658243156991',
+        clientSecret: 'b14fda576d40804887de6272804148c7'
+    }
 }
